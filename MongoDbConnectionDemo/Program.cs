7bf82d0b0263b1e1fefeeaa891fc20270a6dbab5
David Larsen <davidplarsen@gmail.com>
2020-11-19 16:09:38 -0600

    Implement Create and Read

--- MongoDbConnectionDemo/Program.cs
+++ MongoDbConnectionDemo/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using MongoDB.Driver;
+using MongoDB.Bson;
 
 namespace MongoDbConnectionDemo
 {
@@ -8,10 +9,11 @@ namespace MongoDbConnectionDemo
         static void Main(string[] args)
         {
             var un = "dLars99";
-            var x = ":rL4ypGkObW75x0nO@cluster0";
-            var db = "sample_airbnb";
+            // Paste in password before use
+            var pw = "";
+            var connDb = "sample_training";
 
-            MongoClient dbClient = new MongoClient($"mongodb+srv://{un}{x}.yivaw.mongodb.net/{db}?retryWrites=true&w=majority");
+            MongoClient dbClient = new MongoClient($"mongodb+srv://{un}{pw}@cluster0.yivaw.mongodb.net/{connDb}?retryWrites=true&w=majority");
 
             var dbList = dbClient.ListDatabases().ToList();
 
@@ -20,6 +22,59 @@ namespace MongoDbConnectionDemo
             {
                 Console.WriteLine(db);
             }
+
+            // Connect to the appropriate database
+            var database = dbClient.GetDatabase("sample_training");
+            // Select the desired collection
+            var collection = database.GetCollection<BsonDocument>("grades");
+
+            // CREATE
+            var document = new BsonDocument
+            {
+                { "student_id", 10000 },
+                { "scores", new BsonArray
+                    {
+                        new BsonDocument{ { "type", "exam"}, { "score", 88.12334193287023 } },
+                        new BsonDocument{ { "type", "quiz"}, { "score", 74.92381029342834 } },
+                        new BsonDocument{ { "type", "homework"}, { "score", 89.97929384290324 } },
+                        new BsonDocument{ { "type", "homework"}, { "score", 82.12931030513218 } },
+                    }
+                },
+                { "class_id", 480 }
+            };
+
+            collection.InsertOne(document);
+
+            // READ
+            // This returns the first document in the collection
+            var firstDocument = collection.Find(new BsonDocument()).FirstOrDefault();
+            Console.WriteLine(firstDocument.ToString());
+
+            // Reading with a filter: create a filter that matches a student_id
+            var filter = Builders<BsonDocument>.Filter.Eq("student_id", 10000);
+            // Pass that filter into a Find command
+            var studentDocument = collection.Find(filter).FirstOrDefault();
+            Console.WriteLine(studentDocument.ToString());
+
+            // Read all/multiple documents
+            var documents = collection.Find(new BsonDocument()).ToList();
+            // With filters - returns documents with a score within the scores array >= 95
+            var highExamScoreFilter = Builders<BsonDocument>.Filter.ElemMatch<BsonValue>(
+                "scores", new BsonDocument { {  "type", "exam" },
+                    { "score", new BsonDocument { {  "$gte", 95 } } }
+            });
+            // Iterate through results
+            var cursor = collection.Find(highExamScoreFilter).ToCursor();
+            foreach (var matchDocument in cursor.ToEnumerable())
+            {
+                Console.WriteLine(matchDocument);
+            }
+
+            // Sorting results
+            var sort = Builders<BsonDocument>.Sort.Descending("student_id");
+            var highestScores = collection.Find(highExamScoreFilter).Sort(sort);
+            // Append .First() to get the top result
+            var highestScore = collection.Find(highExamScoreFilter).Sort(sort).First();
         }
     }
 }
